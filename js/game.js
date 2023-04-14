702ccb5c0fec15cb9bbb6f46fe6195d532488a58
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-14 17:22:31 +0000

    added config in game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
+/* global Phaser */
+
 // Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
 // Created by: Jaden Plugowsky
 // Created on: April 2023
-// This file contains the JS functions for index.html
+// This is the Phaser3 game configuration file
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
