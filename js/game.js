f863c4abe6dde4a69b2bd90ad720a2ac338356da
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-21 16:39:31 +0000

    added physics, centering to game.js

--- js/game.js
+++ js/game.js
@@ -6,11 +6,25 @@
 // Created on: April 2023
 // This is the Phaser3 game configuration file
 
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background colour
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // We place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
