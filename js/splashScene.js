891380f8340dd9773902e2a28c0361904ee8a8a0
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-27 16:54:37 +0000

    splashScene image added

--- js/splashScene.js
+++ js/splashScene.js
@@ -33,6 +33,7 @@ class SplashScene extends Phaser.Scene {
    */
   preload () {
     console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
   }
 
   /**
@@ -41,7 +42,9 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   create (data) {
-    // pass
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   /**
@@ -51,7 +54,9 @@ class SplashScene extends Phaser.Scene {
    * @param {number} delta - The delta time in ms since the last frame.
    */
   update (time, delta) {
+    if (time > 3000) {
     this.scene.switch('titleScene')
+    }
   }
 }
 
