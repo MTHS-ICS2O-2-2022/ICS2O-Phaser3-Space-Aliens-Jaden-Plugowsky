48d69f8133a214bdea516e79c9c826630242cd95
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-10 17:28:56 +0000

    added spaceShip movement

--- js/gameScene.js
+++ js/gameScene.js
@@ -61,7 +61,24 @@ class GameScene extends Phaser.Scene {
    * @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
-    // pass
+    // called 60 times a second, hopefully!
+
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 1920
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 0
+      }
+    }
   }
 }
 
