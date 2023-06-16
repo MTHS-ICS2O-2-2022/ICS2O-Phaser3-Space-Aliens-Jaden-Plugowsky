8832eb32fb9e05009d936867f75bc77e2febb76c
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-06-16 13:52:58 +0000

    final website

--- js/gameScene.js
+++ js/gameScene.js
@@ -18,7 +18,6 @@ class GameScene extends Phaser.Scene {
     const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
-    this.alienGroup.add(anAlien)
   }
 
   /**
